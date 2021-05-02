902757661eb4f16b964d81cece2f67e5e2064339
iivanabm <ivanammarinho@hotmail.com>
2021-05-01 21:09:16 -0300

    Post new category and find category by code

--- backend/src/main/java/com/money/api/resource/CategoryResource.java
+++ backend/src/main/java/com/money/api/resource/CategoryResource.java
@@ -1,11 +1,19 @@
 package com.money.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.money.api.model.Category;
 import com.money.api.repository.CategoryRepository;
@@ -20,5 +28,23 @@ public class CategoryResource {
 	@GetMapping
 	public List<Category> listAll(){
 		return categoryRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category newCategory = categoryRepository.save(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{code}")
+			.buildAndExpand(newCategory.getCode()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(newCategory);
+	}
+	
+	@GetMapping("/{code}")
+	public ResponseEntity<Category> findByCode(@PathVariable Long code) {
+		return this.categoryRepository.findById(code)
+				.map(category -> ResponseEntity.ok(category))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
