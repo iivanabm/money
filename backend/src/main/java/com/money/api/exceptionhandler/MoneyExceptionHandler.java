ddc54f9c590bbd81c7775cdda0578adbac273d70
iivanabm <ivanammarinho@hotmail.com>
2021-05-06 18:38:51 -0300

    Delete person and catch EmptyResultDataAccessException

--- backend/src/main/java/com/money/api/exceptionhandler/MoneyExceptionHandler.java
+++ backend/src/main/java/com/money/api/exceptionhandler/MoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +16,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@Autowired
 	private MessageSource messageSource;
@@ -40,6 +43,16 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
 		List<Error> errors = createErrorList(ex.getBindingResult());
 		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("resource.not-found", null, LocaleContextHolder.getLocale());
+		String devMessage = ex.toString();
+		List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
+		
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 
 	private List<Error> createErrorList(BindingResult bindingResult) {
--- backend/src/main/java/com/money/api/resource/PersonResource.java
+++ backend/src/main/java/com/money/api/resource/PersonResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.api.event.CreatedResourceEvent;
@@ -44,4 +46,11 @@ public class PersonResource {
 		Optional<Person> person = personRepository.findById(code);
 		return person.isPresent() ? ResponseEntity.ok(person.get()) : ResponseEntity.notFound().build();
 	}
+	
+	@DeleteMapping("/{code}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long code) {
+		personRepository.deleteById(code);
+	}
+	
 }
