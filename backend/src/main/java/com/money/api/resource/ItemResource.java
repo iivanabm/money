ab0568d269ef06fcc72bd05d8df0868d98f51dc5
iivanabm <ivanammarinho@hotmail.com>
2021-05-14 13:55:15 -0300

    Add delete items by id

--- backend/src/main/java/com/money/api/resource/ItemResource.java
+++ backend/src/main/java/com/money/api/resource/ItemResource.java
@@ -1,8 +1,8 @@
 package com.money.api.resource;
 
 import java.util.Arrays;
-import java.util.List;
 
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -13,12 +13,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.api.event.CreatedResourceEvent;
@@ -65,6 +67,12 @@ public class ItemResource {
 		return itemRepository.findById(code)
 				.map(item -> ResponseEntity.ok(item))
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@DeleteMapping("/{code}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteItem(@PathVariable Long code){
+		itemRepository.deleteById(code);
 	}
 	
 	@ExceptionHandler({InactivePersonException.class})
